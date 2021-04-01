9eceab5daeeda7f40d1a5cf33b708e25924ad2a9
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-01 21:52:48 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -3,25 +3,25 @@ try:
 except:
     class ExecVars:
         # Set true if its VPS
-        IS_VPS = False
+        IS_VPS = True
         
-        API_HASH = ""
-        API_ID = 0
-        BOT_TOKEN = ""
-        BASE_URL_OF_BOT = ""
+        API_HASH = "a4c9a18cf4d8cb24062ff6916597f832"
+        API_ID = 924859
+        BOT_TOKEN = "1746903731:AAGTudN-XuxhzgbVhBkELv3k35s4pxNG9xg"
+        BASE_URL_OF_BOT = "https://18.218.126.71/"
 
         # Edit the server port if you want to keep it default though.
         SERVPORT = 80
 
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = []
-        OWNER_ID = 0
+        ALD_USR = [-1001399288270, 1002182448]
+        OWNER_ID = 1002182448
         
         # Google Drive Index Link should include the base dir also See readme for more info
-        GD_INDEX_URL = False
+        GD_INDEX_URL = True
 
         # Time to wait before edit message
-        EDIT_SLEEP_SECS = 40
+        EDIT_SLEEP_SECS = 5
 
         # Telegram Upload Limit (in bytes)
         TG_UP_LIMIT = 1700000000
@@ -50,7 +50,7 @@ except:
 
         # Will be enabled once its set
         # For vps change it to True if config loaded
-        RCLONE_ENABLED = False
+        RCLONE_ENABLED = True
 
         # If the user fails to select whether to use rclone or telegram to upload this will be the deafult.
         DEFAULT_TIMEOUT = "leech"
@@ -59,10 +59,10 @@ except:
         RCLONE_CONFIG = False
         
         # Name of the RCLONE drive from the config
-        DEF_RCLONE_DRIVE = ""
+        DEF_RCLONE_DRIVE = "s3"
 
         # Max size of a playlist that is allowed (Number of videos)
-        MAX_YTPLAYLIST_SIZE = 20
+        MAX_YTPLAYLIST_SIZE = 50
         
         # Max size of the torrent allowed
         MAX_TORRENT_SIZE = 10
@@ -75,14 +75,14 @@ except:
         STATUS_DEL_TOUT = 20
 
         # Allow the user settings to be accessed in private
-        USETTINGS_IN_PRIVATE = False
+        USETTINGS_IN_PRIVATE = True
 
         # Torrent max time to collect metadata in seconds
         TOR_MAX_TOUT = 180
 
         # This is to stop someone from abusing the system by imposing the limit
         # [<GBs of total torrent sapce>, <Number of youtube videos allowed to download>, <Number of youtube playlists allowed to download>]
-        USER_CAP_ENABLE = False
+        USER_CAP_ENABLE = True
         USER_CAP_LIMIT = [50,10,2]
 
         # No need to worry about these
@@ -94,8 +94,3 @@ except:
         METAINFO_BOT = False
         EXPRESS_UPLOAD = True
         
-
-
-
-
-
